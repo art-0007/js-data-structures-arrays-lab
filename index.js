641ca16dab45b96c6fb892247ca241ed2931ec15
Artur <info@aigroupusa.com>
2021-11-30 13:01:01 -0500

    commit tree 5b4ecfe71017ecc1d7162b744732d7e2c927b659

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+    drivers.push(name)
+  }
+
+  function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+  }
+
+  function destructivelyRemoveLastDriver(name){
+    drivers.pop(name)
+  }
+
+  function destructivelyRemoveFirstDriver(name){
+    drivers.shift(name)
+  }
+
+  function appendDriver(name){
+    let new_drivers = [...drivers, name]
+    return new_drivers
+  }
+
+  function prependDriver(name){
+    let new_drivers = [name, ...drivers]
+    return new_drivers
+  }
+
+  function removeLastDriver(){
+    return drivers.slice(0, drivers.length - 1)
+  }
+
+  function removeFirstDriver(){
+    return drivers.slice(1)
+  }
+  
